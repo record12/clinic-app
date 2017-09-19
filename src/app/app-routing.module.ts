866a5d06d669adb31016d6ba40f54b10aa4cd435
xdxxxd <ed1904@yadex.ru>
2017-09-19 15:51:37 +0300

    added routing, a header component, a dummy clinic component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ClinicsComponent } from './components/clinics/clinics.component';
+import { NoContentComponent } from './components/no-content/no-content.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/clinics', pathMatch: 'full'},
+  { path: 'clinics', component: ClinicsComponent },
+  { path: '**', component: NoContentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ClinicsComponent } from './components/clinics/clinics.component';
+import { NoContentComponent } from './components/no-content/no-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ClinicsComponent,
+    NoContentComponent
   ],
   imports: [
     BrowserModule,
